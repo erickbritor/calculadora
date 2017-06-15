5b887aeee31aa26c4fc1608e6e9edc37fc41f315
matos <almir.ferreira@infouniron.com>
2017-06-15 18:17:13 -0400

    criação dos atributos

---
+++ Calculadora/Calculadora/Classes/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Classes
+{
+    class Calculadora
+    {
+        private double numero1;
+        private double numero2;
+        private double total;
+
+        public double Numero1 { get => numero1; set => numero1 = value; }
+        public double Numero2 { get => numero2; set => numero2 = value; }
+        public double Total { get => total; set => total = value; }
+    }
+}
