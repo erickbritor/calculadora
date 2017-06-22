a54c394bec6cd99b6b604a439388bce8220240b6
erickbritor <erick.rayan@infouniron.com>
2017-06-21 23:41:07 -0400

    Formulario Layout

--- Calculadora/Calculadora/Form1.cs
+++ Calculadora/Calculadora/Form1.cs
@@ -16,5 +16,15 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
